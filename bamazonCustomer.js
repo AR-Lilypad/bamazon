837e75b9af7fc7bae771fb2c0a1500f5cd01da02
AR-Lilypad <angie-for-seekers@msn.com>
2019-11-07 19:33:00 -0500

    made table with products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,55 +17,47 @@ let connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
-    // shop();
+   
 });
 
 function bamTable() {
     // query the database for items being purchased
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
-        // create a display table using cli-table taken from npm site
-        const table = new Table({
-            head: ["ID", "Prodcuct Name", "Department", "Price", "Qty Available"],
-            colWidths: [5, 100, 15, 10]
-        });
-        // display table of products
-        for (const i = 0; i < results.length; i++) {
-            table.push([results[i].id, results[i].product_name, results[i].department_name, results[i].price, results[i].quantity]);
-        }
-        console.log(table.toString());
+        console.log( "\n");
         console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" + "\n");
         console.log("Welcome to the newest shopping site: Bamazon");
         console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" + "\n");
+        console.table(results);
     });
 }
 bamTable();
 
 // let the shopping begin- create function to begin the query
-// function shop(){
-//     inquirer
-//         .prompt([
-//             {
-//                 name: "id",
-//                 type: "input",
-//                 message: "What would you like to purchase? Please select by item's ID numbers."
-//             },
-//             {
-//                 name: "quantity",
-//                 type: "input",
-//                 message: "How many units would you like to purchase?"
-//             }
-//         ])
-//         .then(function (answer) {
-//             // get the information of the shoppers 'cart'
-//             let itemSelected = 
-//             for (var i = 0; i < results.length; i++) {
-//                 if (results[i].id === answer.id) {
-//                     itemSelected = results[i];
-//                 }
-//             }
-//         })
-// }
+function shop(){
+    inquirer
+        .prompt([
+            {
+                name: "id",
+                type: "input",
+                message: "What would you like to purchase? Please select by item's ID numbers."
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many units would you like to purchase?"
+            }
+        ])
+        .then(function (answer) {
+            // get the information of the shoppers 'cart'
+            let itemSelected = 
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].id === answer.id) {
+                    itemSelected = results[i];
+                }
+            }
+        })
+}
 
 
 
