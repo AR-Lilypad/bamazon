024d3dd09524edb644876a56f576b2c50127c42e
Angie Roach <angieroach4@gmail.com>
2019-11-10 21:45:27 -0500

    added select item function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -31,7 +31,7 @@ function bamTable() {
         });
         // display table of products
         for (const i = 0; i < results.length; i++) {
-            table.push([results[i].id, results[i].product_name, results[i].department_name, results[i].price, results[i].quantity]);
+            table.push([result[i].id, result[i].product_name, result[i].department_name, result[i].price, result[i].quantity]);
         }
         console.log(table.toString());
         console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" + "\n");
@@ -42,31 +42,42 @@ function bamTable() {
 bamTable();
 
 // let the shopping begin- create function to begin the query
-// function shop(){
-//     inquirer
-//         .prompt([
-//             {
-//                 name: "id",
-//                 type: "input",
-//                 message: "What would you like to purchase? Please select by item's ID numbers."
-//             },
-//             {
-//                 name: "quantity",
-//                 type: "input",
-//                 message: "How many units would you like to purchase?"
-//             }
-//         ])
-//         .then(function (answer) {
-//             // get the information of the shoppers 'cart'
-//             let itemSelected = 
-//             for (var i = 0; i < results.length; i++) {
-//                 if (results[i].id === answer.id) {
-//                     itemSelected = results[i];
-//                 }
-//             }
-//         })
-// }
+function shop() {
+    inquirer
+        .prompt([
+            {
+                name: "id",
+                type: "input",
+                message: "What would you like to purchase? Please select by item's ID numbers."
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many units would you like to purchase?"
+            }
+        ])
+        .then(function (answer) {
+            // get the information of the shoppers 'cart'
+            let itemSelected = (answer.id) - 1;
+            let howMany = parseInt(answer.quantity);
+            let total = parseFloat(((result[itemSelected.Price]) * howMany).toFixed(2));
 
+            //check stock
+            if (result[itemSelected].quantity >= howMany) {
+                connection.query("UPDATE products SET ? WHERE ?",
+                    [{ Quantity: (result[itemSelected].Quantity - howMany) }, { ID: answer.id }],
+
+                    function (err, result) {
+                        if (err) throw err;
+                        console.log("Thank you! Your items are available and your total is $ " + total.toFixed(2) + ". Your items will be shipped in 2 days");
+                    });
+            }
+        )}
+}
+
+
+
+// itemSelected = result[i];
 
 
 // // connect to the mysql server and sql database then run the prompt
